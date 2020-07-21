88b7b7321627f764db118b708ecfc024181e49f1
christopher <christopher.fernandes@before.com.br>
2020-07-21 19:35:03 -0400

    implemento método para fragmentar o arquivo

--- encoder/application/services/video_service.go
+++ encoder/application/services/video_service.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 )
 
 type VideoService struct {
@@ -54,4 +55,29 @@ func (v *VideoService) Download(bucketName string) error {
 	log.Printf("video %v foi salvo", v.Video.ID)
 
 	return nil
+}
+
+func (v VideoService) Fragment() error {
+	err := os.Mkdir(os.Getenv("LOCAL_STORAGE_PATH")+"/"+v.Video.ID, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	source := os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".mp4"
+	target := os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".frag"
+
+	cmd := exec.Command("mp4fragment", source, target)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+
+	printOutput(output)
+	return nil
+}
+
+func printOutput(out []byte) {
+	if len(out) > 0 {
+		log.Printf("=========> Output: %s\n", string(out))
+	}
 }
