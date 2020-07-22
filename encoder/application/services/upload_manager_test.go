9797f07f90c646789355de1ca87f1ab2fb3d19aa
christopher <christopher.fernandes@before.com.br>
2020-07-21 22:58:50 -0400

    implementando test de upload de video no GCP

--- encoder/application/services/upload_manager_test.go
+++ encoder/application/services/upload_manager_test.go
@@ -1,122 +1,47 @@
-package services
+package services_test
 
 import (
-	"cloud.google.com/go/storage"
-	"context"
-	"io"
+	"encoder/application/services"
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/require"
 	"log"
 	"os"
-	"path/filepath"
-	"runtime"
-	"strings"
+	"testing"
 )
 
-type VideoUpload struct {
-	Paths        []string
-	VideoPath    string
-	OutputBucket string
-	Errors       []string
-}
-
-func NewVideoUpload() *VideoUpload {
-	return &VideoUpload{}
-}
-
-func (vu *VideoUpload) UploadObject(objectPath string, client *storage.Client, ctx context.Context) error {
-	path := strings.Split(objectPath, os.Getenv("LOCAL_STORAGE_PATH")+"/")
-	f, err := os.Open(objectPath)
+func init() {
+	err := godotenv.Load("../../.env")
 	if err != nil {
-		return err
+		log.Fatalf("erro ao carregar o arquivo .env")
 	}
-
-	defer f.Close()
-
-	wc := client.Bucket(vu.OutputBucket).Object(path[1]).NewWriter(ctx)
-	wc.ACL = []storage.ACLRule{{Entity: storage.AllUsers, Role: storage.RoleReader}}
-
-	if _, err = io.Copy(wc, f); err != nil {
-		return err
-	}
-
-	if err := wc.Close(); err != nil {
-		return err
-	}
-
-	return nil
 }
 
-func (vu *VideoUpload) loadPaths() error {
-	err := filepath.Walk(vu.VideoPath, func(path string, info os.FileInfo, err error) error {
-		if !info.IsDir() {
-			vu.Paths = append(vu.Paths, path)
-		}
-		return nil
-	})
+func TestVideoServiceUpload(t *testing.T) {
 
-	if err != nil {
-		return err
-	}
+	video, repo := prepare()
 
-	return nil
-}
+	videoService := services.NewVideoService()
+	videoService.Video = video
+	videoService.VideoRepository = repo
 
-func (vu *VideoUpload) ProcessUpload(concurrency int, doneUpload chan string) error {
-	in := make(chan int, runtime.NumCPU()) //arquivo baseado na posicao do slice Paths
-	returnChannel := make(chan string)
+	err := videoService.Download("codeflix")
+	require.Nil(t, err)
 
-	err := vu.loadPaths()
-	if err != nil {
-		return err
-	}
+	err = videoService.Fragment()
+	require.Nil(t, err)
 
-	uploadCliente, ctx, err := getClientUpload()
-	if err != nil {
-		return err
-	}
+	err = videoService.Encode()
+	require.Nil(t, err)
 
-	for process := 0; process > concurrency; process++ {
-		go vu.uploadWorker(in, returnChannel, uploadCliente, ctx)
-	}
+	videoUpload := services.NewVideoUpload()
+	videoUpload.OutputBucket = "codeflix"
+	videoUpload.VideoPath = os.Getenv("LOCAL_STORAGE_PATH") + "/" + video.ID
 
-	go func() {
-		for x := 0; x < len(vu.Paths); x++ {
-			in <- x
-		}
-		close(in)
-	}()
+	doneUpload := make(chan string)
+	go videoUpload.ProcessUpload(50, doneUpload)
+	result := <-doneUpload
+	require.Equal(t, result, "upload realizado")
 
-	for r := range returnChannel {
-		if r != "" {
-			doneUpload <- r
-			break
-		}
-	}
-
-	return nil
-}
-
-func (vu *VideoUpload) uploadWorker(in chan int, returnChan chan string, uploadCliente *storage.Client, ctx context.Context) {
-	for x := range in {
-		err := vu.UploadObject(vu.Paths[x], uploadCliente, ctx)
-
-		if err != nil {
-			vu.Errors = append(vu.Errors, vu.Paths[x])
-			log.Printf("erro ao realizar o upload: %v. Erro: %v", vu.Paths[x], err)
-			returnChan <- err.Error()
-		}
-		returnChan <- ""
-	}
-
-	returnChan <- "upload realizado"
-}
-
-func getClientUpload() (*storage.Client, context.Context, error) {
-	ctx := context.Background()
-
-	client, err := storage.NewClient(ctx)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return client, ctx, nil
+	//err = videoService.Finish()
+	//require.Nil(t, err)
 }
